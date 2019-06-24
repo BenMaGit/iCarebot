e7070f5ec99efbe350213b1918b82dc3540a9b51
ben810514@gmail.com <ben810514@gmail.com>
2019-06-24 23:09:03 +0800

    third try

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const linebot = require('linebot');
+const express = require('express')
 
 const bot = linebot({
     channelId: '1591861988',
@@ -7,15 +8,23 @@ const bot = linebot({
 })
 
 bot.on('message', function (event) {
-    // event.message.text是使用者傳給bot的訊息
-    // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
     event.reply(event.message.text).then(function (data) {
-      // 當訊息成功回傳後的處理
+     
     }).catch(function (error) {
-      // 當訊息回傳失敗後的處理
+
     });
   });
 
-  bot.listen('/linewebhook', 3000, function () {
-    console.log('[BOT已準備就緒]');
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+var port = server.address().port;
+  console.log("App now running on port", port);
+});
+
+bot.listen('/linewebhook', 3000, function () {
+    console.log('BOT is ready');
 });
