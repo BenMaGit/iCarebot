53d633ca6d001d79fb64cabc3dc93b61abafb665
ben810514@gmail.com <ben810514@gmail.com>
2019-06-25 08:41:37 +0800

    test push api

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ app.post('/', linebotParser);
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function() {
-var port = server.address().port;
-  console.log("App now running on port", port);
+    var port = server.address().port;
+    console.log("App now running on port", port);
 });
 
 bot.listen('/linewebhook', 3000, function () {
