f943ad6ebae2262ac89febb0b67789d2aa3ccceb
ben810514@gmail.com <ben810514@gmail.com>
2019-07-24 09:13:34 +0800

    testing

--- utils/replyHandler.js
+++ utils/replyHandler.js
@@ -1,6 +1,10 @@
-const configs = require('../configs')
+
 const linebot = require('linebot');
-const bot = linebot(configs.botAccess);
+const bot = linebot({
+    channelId: process.env.channelId,
+    channelSecret: process.env.channelSecret,
+    channelAccessToken: process.env.channelAccessToken
+});
 
 function replyHandler(event, msg){
     return event.reply(msg)
